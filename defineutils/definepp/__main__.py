53a220b86fc1cdca337b506e44b236808c73dcf3
swhume <swhume@gmail.com>
2026-07-10 10:11:58 -0400

    adds clean exit on ctrl-c

--- defineutils/definepp/__main__.py
+++ defineutils/definepp/__main__.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from pathlib import Path
 import argparse
 from defineutils.definepp import DefinePrettyPrinter, DefinePrettyPrintError
@@ -12,6 +14,23 @@ def main():
             pp.pretty_print_to_console(head=args.head)
     except DefinePrettyPrintError as e:
         print(e)
+    except BrokenPipeError:
+        # the reader (e.g. more/less/head) closed the pipe early
+        _silence_stdout()
+        sys.exit(0)
+    except KeyboardInterrupt:
+        # the user pressed Ctrl-C (e.g. to quit the pager)
+        _silence_stdout()
+        sys.exit(130)
+
+def _silence_stdout():
+    """Redirect stdout's fd to os.devnull so the interpreter's shutdown flush of any
+    remaining buffered output does not raise a second BrokenPipeError. Best effort."""
+    try:
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+    except OSError:
+        pass
 
 def set_cmd_line_args():
     """
